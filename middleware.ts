c84ef615e38939b86afd265cd539e62fb21f999f
Mavegari <mavega_95@hotmail.com>
2026-05-07 10:15:45 +0200

    Cambio en middleware

--- middleware.ts
+++ middleware.ts
@@ -1,14 +1,7 @@
-// src/middleware.ts (o en la raíz, si no usas carpeta src)
+// middleware.ts (en la raíz)
 import { NextResponse } from 'next/server'
 import type { NextRequest } from 'next/server'
 
 export function middleware(request: NextRequest) {
-  // Deja pasar todas las peticiones (por ahora)
   return NextResponse.next()
-}
-
-export const config = {
-  matcher: [
-    '/((?!api|_next/static|_next/image|favicon.ico).*)',
-  ],
 }
