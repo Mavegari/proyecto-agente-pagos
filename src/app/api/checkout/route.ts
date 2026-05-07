362ea85a098d4c380c378cb204798f069808bd7c
Mavegari <mavega_95@hotmail.com>
2026-05-07 09:51:49 +0200

    Cambios en rutas y archivos

--- src/app/api/checkout/route.ts
+++ src/app/api/checkout/route.ts
@@ -2,7 +2,7 @@ import { NextResponse } from 'next/server'
 import Stripe from 'stripe'
 
 const stripe = new Stripe(process.env.STRIPE_SECRET_KEY!, {
-  apiVersion: '2024-12-18.acacia' as any,
+  apiVersion: '2024-04-10' as any,
 })
 
 export async function POST(req: Request) {
